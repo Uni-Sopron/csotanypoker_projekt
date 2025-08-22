060153d804bae1055b98d8d7522046a03e187aa3
Skriba-Izabella <czvlwt@uni-sopron.hu>
2025-08-22 09:37:39 +0200

    Loading

--- csotanypoker/client/loading_screen.py
+++ csotanypoker/client/loading_screen.py
@@ -1,6 +1,6 @@
 from csotanypoker.client.base_screen import BaseScreen
-from csotanypoker.client.constans import BLACK, WHITE
-from csotanypoker.client.drawing_helpers import draw_text
+from csotanypoker.client.constans import DARK_GREEN
+from csotanypoker.client.drawing_helpers import draw_text, load_background
 
 
 class LoadingScreen(BaseScreen):
@@ -9,13 +9,21 @@ class LoadingScreen(BaseScreen):
 
     def draw(self):
         """Draw the loading screen"""
-        self.client.window.fill(WHITE)
-        draw_text(self.client.window, "Betöltés...", BLACK, 400, 300, True)
-        print("Betöltés...")
+        load_background(self.client.width, self.client.height, self.client.window)
+        draw_text(
+            self.client.window,
+            "Betöltés...",
+            DARK_GREEN,
+            self.client.width // 2,
+            self.client.height // 2,
+            True,
+            "bold",
+            90,
+        )
 
     def handle_mouse_click(self, pos):
         """No interaction on loading screen"""
-        return False
+        pass
 
     def handle_key_press(self, event):
         """No interaction on loading screen"""
