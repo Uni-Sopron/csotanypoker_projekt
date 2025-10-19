e77c82ba540cfbaca50bfda5ef9d2a5d4a6fc329
Skriba-Izabella <czvlwt@uni-sopron.hu>
2025-10-19 23:34:37 +0200

    gamestate kivétele a modellböl

---
+++ csotanypoker/models/clientgamestate.py
@@ -0,0 +1,37 @@
+from pydantic import BaseModel, Field, field_serializer
+from typing import Optional,  Set
+
+from csotanypoker.models.animal import Animal
+
+
+class AbstractGameState(BaseModel):
+    game_id: Optional[str] = Field(None, description="Játék egyedi azonosítója")
+    room_id: Optional[str] = Field(None, description="Szoba azonosítója")
+    question_card: Optional[Animal] = Field(None, description="Kérdéses kártya")
+    visited_already: Optional[Set[str]] = Field(
+        set(), description="Azoknak a neve akiknél már volt a kérdéses kártya"
+    )
+    voters: Optional[Set[str]] = Field(set(), description="Szavazók nevei")
+
+
+class ClientGameState(AbstractGameState):
+    active_player: Optional[str] = Field(None, description="Aktív játékos neve")
+    targeted_player: Optional[str] = Field(None, description="Célzott játékos neve")
+
+    @field_serializer("question_card")
+    def serialize_question_card(self, value):
+        if value is None:
+            return None
+        return value.value
+
+    @field_serializer("voters")
+    def serialize_voters(self, value):
+        if value is None:
+            return None
+        return list(value)
+
+    @field_serializer("visited_already")
+    def serialize_visited_already(self, value):
+        if value is None:
+            return None
+        return list(value)
